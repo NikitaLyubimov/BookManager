01837b7ead7a148d12d11c4c164d64835138f506
NikitaLyubimov <sobachiy.deputat2017@yandex.ru>
2019-10-12 18:05:53 +0300

    Implemented Search method for MainPageViewModel

---
+++ BookManager/Models/BookPartial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManager.Models
+{
+    public partial class Book
+    {
+        private BookManagerDbEntities _dbContext = new BookManagerDbEntities();
+
+        public string AuthorName
+        {
+            get
+            {
+                List<BookAuthor> bookAuthor = BookAuthor.Where(x => x.Book.Title.Contains(Title)).ToList();
+                string author = "";
+
+                if (bookAuthor.Count > 1)
+                {
+                    foreach (BookAuthor ba in bookAuthor)
+                    {
+                        author += $"{ba.Author.Name}, ";
+                    }
+                    return author;
+                }
+                else
+                    return bookAuthor[0].Author.Name;
+                   
+            }
+        }
+    }
+}
--- BookManager/ViewModels/MainPageViewModel.cs
+++ BookManager/ViewModels/MainPageViewModel.cs
@@ -5,8 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-
-
+using System.Windows.Input;
 using BookManager.Models;
 
 namespace BookManager.ViewModels
@@ -18,7 +17,7 @@ namespace BookManager.ViewModels
         private string _author;
         private string _subject;
         private RelayCommand _searchCommand;
-        private ObservableCollection<Book> _bookList;
+        private List<BookAuthor> _bookList;
         private BookManagerDbEntities _dbContext;
         #endregion
 
@@ -27,48 +26,101 @@ namespace BookManager.ViewModels
         public MainPageViewModel()
         {
             _dbContext = new BookManagerDbEntities();
+
+            BookList = _dbContext.BookAuthor.ToList();
         }
 
         #endregion
         #region Public fields
+        /// <summary>
+        /// Field fore searching book
+        /// </summary>
         public string Title
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
 
+        /// <summary>
+        /// Field for searching book
+        /// </summary>
         public string Author
         {
             get { return _author; }
             set { SetProperty(ref _author, value); }
         }
 
+        /// <summary>
+        /// Field for searching book
+        /// </summary>
         public string Subject
         {
             get { return _subject; }
             set { SetProperty(ref _subject, value); }
         }
 
-        public ObservableCollection<Book> BookList
+        /// <summary>
+        /// List with books 
+        /// </summary>
+        public List<BookAuthor> BookList
         {
             get { return _bookList; }
             set { SetProperty(ref _bookList, value); }
         }
+
+        /// <summary>
+        /// Command for searching
+        /// </summary>
+        public ICommand SearchCommand
+        {
+            get
+            {
+                if (_searchCommand == null)
+                    _searchCommand = new RelayCommand(Search);
+                return _searchCommand;
+            }
+        }
         #endregion
 
+        /// <summary>
+        /// Method which search books depending on user request
+        /// </summary>
         private void Search()
         {
-            if (string.IsNullOrEmpty(Title) || string.IsNullOrEmpty(Author) || string.IsNullOrEmpty(Subject))
+            if (string.IsNullOrEmpty(Title) && string.IsNullOrEmpty(Author) && string.IsNullOrEmpty(Subject))
                 MessageBox.Show("At least one field should be entered");
             else
             {
-                if (string.IsNullOrEmpty(Author) && string.IsNullOrEmpty(Subject))
+                if (string.IsNullOrEmpty(Author) && string.IsNullOrEmpty(Subject) && !string.IsNullOrEmpty(Title))
+                    BookList = _dbContext.BookAuthor.Where(x => x.Book.Title.Contains(Title)).ToList();
 
-                    BookList = new ObservableCollection<Book>(_dbContext.Book.Where(x => x.Title.Contains(Title)).ToList());
-                else if (string.IsNullOrEmpty(Subject))
-                    BookList = new ObservableCollection<Book>(_dbContext.Book.Where(x => x.Title.Contains(Title) && x.).ToList());
+                else if (string.IsNullOrEmpty(Subject) && !string.IsNullOrEmpty(Author) && !string.IsNullOrEmpty(Title))
+                    BookList = _dbContext.BookAuthor.Where(x => x.Book.Title.Contains(Title) && x.Author.Name.Contains(Author)).ToList();
+                else if (!string.IsNullOrEmpty(Title) && !string.IsNullOrEmpty(Subject) && string.IsNullOrEmpty(Author))
+                {
+                    BookList = _dbContext.BookSubject.Where(x => x.Book.Title.Contains(Title) && x.Subject.Contains(Subject))
+                        .Select(x => x.Book)
+                        .Select(x => x.BookAuthor) as List<BookAuthor>;
+                }
+                else if (!string.IsNullOrEmpty(Title) && !string.IsNullOrEmpty(Subject) && !string.IsNullOrEmpty(Author))
+                {
+                    BookList = _dbContext.BookSubject.Where(x => x.Book.Title.Contains(Title) && x.Subject.Contains(Subject)
+                    && x.Book.BookAuthor.FirstOrDefault(y => y.Author.Name.Contains(Author)) != null)
+                        .Select(x => x.Book)
+                        .Select(x => x.BookAuthor) as List<BookAuthor>;
+                }
+                else if (!string.IsNullOrEmpty(Author) && !string.IsNullOrEmpty(Subject) && string.IsNullOrEmpty(Title))
+                {
+                    BookList = _dbContext.BookSubject.Where(x => x.Book.BookAuthor.FirstOrDefault(y => y.Author.Name.Contains(Author)) != null && x.Subject.Contains(Subject))
+                        .Select(x => x.Book)
+                        .Select(x => x.BookAuthor) as List<BookAuthor>;
+                }
+
+
             }
         }
+
+
 
 
     }
