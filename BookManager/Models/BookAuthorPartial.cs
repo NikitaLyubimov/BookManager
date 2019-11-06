563e2d0f897bc22104ac6045edd03c6a4a75c6bb
NikitaLyubimov <sobachiy.deputat2017@yandex.ru>
2019-11-06 21:58:28 +0300

    Split up authors with comma

---
+++ BookManager/Models/BookAuthorPartial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManager.Models
+{
+    public partial class BookAuthor
+    {
+        public string AuthorName
+        {
+            get
+            {
+                BookManagerDbEntities dbContext = new BookManagerDbEntities();
+                List<BookAuthor> books = dbContext.BookAuthor.Where(x => x.BookKey == BookKey).ToList();
+
+                StringBuilder sb = new StringBuilder();
+
+                if (books.Count() > 1)
+                {
+                    for(int i = 0;i < books.Count(); i++)
+                    {
+                        if (i == books.Count() - 1)
+                            sb.Append(books[i].Author.Name);
+                        else
+                            sb.Append($"{books[i].Author.Name}, ");
+                    }
+                }
+                else
+                {
+                    sb.Append(books[0].Author.Name);
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
